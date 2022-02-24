3cbe12c51eda8d541f2b42f9ae23529a154a4822
PRCSGH <78178853+PRCSGH@users.noreply.github.com>
2022-02-24 16:47:57 -0400

    Add files via upload

---
+++ correa_accounting_reports_aged_payable/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import aged_payable_report
+from . import aged_partner_balance
+from . import cash_flow_report
+from . import consolidated_journals
+from . import general_ledger
+from . import multicurrency_revaluation_report
+from . import partner_ledger
